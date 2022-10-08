35045a112dee5668979693050709a8e481d5925c
a5medHub <ayassine.auce@gmail.com>
2022-10-08 08:14:56 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hello World - testing");
+  res.render("index");
 });
 
 module.exports = router;
